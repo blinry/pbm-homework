e23b994147e55cea7906f4f91d783e0a360a55f7
Sebastian Morr <sebastian@morr.cc>
2013-11-11 09:55:32 +0100

    Refactored code, comments for 2.1

--- ex02/RungeKuttaSolver.cpp
+++ ex02/RungeKuttaSolver.cpp
@@ -2,77 +2,45 @@
 using namespace std;
 
 void RungeKuttaSolver::step(const Time stepsize)  {
-	/* Please fill in your code for computing a 4th order Runge-Kutta time step here.
-	 * The system on which the solver operates is available in the variable "system".
-	 *
-	 * The system of differential equations that you will solve is of the form
-	 *
-	 *   y'(t) = f(y(t)),
-	 *
-	 * where
-	 *
-	 *   y(t) = (x(t), v(t)),
-	 *
-	 * with x(t) and v(t) containing the positions and velocities of all the
-	 * particles, respectively. You can split this system into the position and
-	 * velocity components, such that
-	 *
-	 *   x'(t) = v(t)  and
-	 *   v'(t) = a(x(t)).
-	 *
-	 * You can evaluate a(x(t)) by setting all the particle positions in the system to
-	 * the corresponding values of x(t), calling _system->computeAccelerations() and
-	 * reading the accelerations back from the particles. If you want to be generic
-	 * and allow for systems where the accelerations may also depend on the velocity
-	 * (for example, when there is friction) or on the time (for example, when an
-	 * external force changes over time), you should also set the particle velocities
-	 * and the system time before calling computeAccelerations().
-	 *
-	 * When your computation is done, store the new x and v values in the particles.
-	 * Do not forget to increment the current time of the system when you are done.
-	 */
-
     vector<Length3D> r1, r2, r3, r4;
     vector<Velocity3D> v1, v2, v3, v4;
     vector<Acceleration3D> a1, a2, a3, a4;
 
+    // store original position and velocity
     for(unsigned int i = 0; i < _system->particles.size(); ++i) {
         Particle* p = &_system->particles[i];
         r1.push_back(p->position);
         v1.push_back(p->velocity);
     }
+    // ... and compute the particles accelerations
     compAcc(r1, &a1);
 
-    for(unsigned int i = 0; i < _system->particles.size(); ++i) {
-        Particle* p = &_system->particles[i];
-        r2.push_back(r1[i]+.5*stepsize*v1[i]);
-        v2.push_back(p->velocity+.5*stepsize*a1[i]);
-    }
+    // do a half explicit Euler step using original velocity and the computed acceleration,
+    // store new positions and velocities in r2 and v2
+    eulerStep(r1, v1, a1, &r2, &v2, stepsize*0.5);
+    // recompute accelerations
     compAcc(r2, &a2);
 
-    for(unsigned int i = 0; i < _system->particles.size(); ++i) {
-        Particle* p = &_system->particles[i];
-        r3.push_back(r1[i]+.5*stepsize*v2[i]);
-        v3.push_back(p->velocity+.5*stepsize*a2[i]);
-    }
+    // another half step: Start at original position and velocity, but use velocity and
+    // acceleration computed in the prior step
+    eulerStep(r1, v2, a2, &r3, &v3, stepsize*0.5);
     compAcc(r3, &a3);
 
-    for(unsigned int i = 0; i < _system->particles.size(); ++i) {
-        Particle* p = &_system->particles[i];
-        r4.push_back(r1[i]+stepsize*v3[i]);
-        v4.push_back(p->velocity+stepsize*a3[i]);
-    }
+    // and another, use full stepsize this time
+    eulerStep(r1, v3, a3, &r4, &v4, stepsize);
     compAcc(r4, &a4);
 
+    // calculate final position and velocity by weighting the individual values
     for(unsigned int i = 0; i < _system->particles.size(); ++i) {
         Particle* p = &_system->particles[i];
-        p->position = r1[i] + stepsize/6.0*(v1[i] + 2*v2[i] + 2*v3[i] + 2*v4[i]);
-        p->velocity = p->velocity + stepsize/6.0*(a1[i] + 2*a2[i] + 2*a3[i] + 2*a4[i]);
+        p->position = r1[i] + stepsize/6.0*(v1[i] + 2*v2[i] + 2*v3[i] + v4[i]);
+        p->velocity = p->velocity + stepsize/6.0*(a1[i] + 2*a2[i] + 2*a3[i] + a4[i]);
     }
 
     _system->time += stepsize;
 }
 
+// compute each particles' acceleration a for positions r. Note: Overwrites the positions!
 void RungeKuttaSolver::compAcc(vector<Length3D> r, vector<Acceleration3D>* a) {
     for(unsigned int i = 0; i < _system->particles.size(); ++i) {
         Particle* p = &_system->particles[i];
@@ -82,5 +50,14 @@ void RungeKuttaSolver::compAcc(vector<Length3D> r, vector<Acceleration3D>* a) {
     for(unsigned int i = 0; i < _system->particles.size(); ++i) {
         Particle* p = &_system->particles[i];
         a->push_back(p->acceleration);
+    }
+}
+
+// do an explicit Euler step, starting at r and the particles' velocity
+void RungeKuttaSolver::eulerStep(vector<Length3D> r, vector<Velocity3D> v, vector<Acceleration3D> a, vector<Length3D>* r2, vector<Velocity3D>* v2, Time stepsize) {
+    for(unsigned int i = 0; i < _system->particles.size(); ++i) {
+        Particle* p = &_system->particles[i];
+        r2->push_back(r[i]+stepsize*v[i]);
+        v2->push_back(p->velocity+stepsize*a[i]);
     }
 }
--- ex02/RungeKuttaSolver.h
+++ ex02/RungeKuttaSolver.h
@@ -19,6 +19,7 @@ class RungeKuttaSolver: public Solver {
 		 */
 		void step(const Time stepsize);
         void compAcc(vector<Length3D> r, vector<Acceleration3D>* a);
+        void eulerStep(vector<Length3D> r, vector<Velocity3D> v, vector<Acceleration3D> a, vector<Length3D>* r2, vector<Velocity3D>* v2, Time stepsize);
 };
 
 #endif
